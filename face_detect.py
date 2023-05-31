8a8ab518279dcf080a0e481ae8cf56b689cfde2a
Shahram Shahraki <shahram.shahraki@gmail.com>
2023-05-31 10:34:10 +0330

    identiy faces on images

--- face_detect.py
+++ face_detect.py
@@ -1,22 +1,12 @@
 import cv2
 import os
 import face_recognition
-
-
-a = [ [2, 4, 6, 8 ], [ 1, 3, 5, 7 ], [ 8, 6, 4, 2 ], [ 7, 5, 3, 1 ] ] 
-          
-for i in range(len(a)) : 
-    for j in range(len(a[i])) : 
-        print(a[i][j], end=" ")
-    print()    
-
-b = [[1,2,3,4,5],[6,7,8,9,10]]
-print(b[2:5])
+import numpy as np
 
 cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
 haar_model = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
 face_cascade = cv2.CascadeClassifier(haar_model)
-
+path_save_faces = 'face.jpg'
 # To capture from camera
 # captured = cv2.VideoCapture(0)
 
@@ -25,29 +15,41 @@ face_cascade = cv2.CascadeClassifier(haar_model)
 captured = cv2.VideoCapture("D:\\west_world.mp4")
 
 base_img = cv2.imread("D:\\work\\github\\face-detect\\ID-Card 1.jpg")
-cv2.imshow('base_img', base_img)
 base_img_encoding = face_recognition.face_encodings(base_img)[0]
 faces = face_cascade.detectMultiScale(base_img,1.1,10)
-face_img=base_img.copy()
+face_img_base=base_img.copy()
 for (x,y,w,h) in faces:
-    face_img = face_img[x:x+w,y:y+h,:]
+    face_img_base = face_img_base[y:y+h, x:x+w]
+    cv2.imshow('Searching for...', face_img_base)
     cv2.rectangle(base_img,(x,y),(x+w,h+y),(255,0,0),2)
-cv2.imshow('base_img_faces', base_img)
-cv2.imshow('face_img', face_img)
 
-
-# test_img = cv2.imread("D:\\work\\github\\face-detect\\IMG_20150925_153809.jpg")
-# test_img_encoding = face_recognition.face_encodings(test_img)[0]
-# faces = face_cascade.detectMultiScale(test_img,1.1,4)
-# for (x,y,w,h) in faces:
-#     cv2.rectangle(test_img,(x,y),(x+w,h+y),(255,0,0),2)
-# cv2.imshow('test_img_faces', test_img)
+test_img = cv2.imread("D:\\work\\github\\face-detect\\IMG_20150925_153809.jpg")
+test_img_encoding = face_recognition.face_encodings(test_img)[0]
+faces = face_cascade.detectMultiScale(test_img,1.1,20)
+i=0
+faces_test=[[0],[0],[0],[0]]
+for (x,y,w,h) in faces:
+    face = test_img[y:y+h, x:x+w]
+    faces_test.insert(i, face)
+    cv2.rectangle(test_img,(x,y),(x+w,h+y),(255,0,0),2)
+    i+=1
+    
+for index in range(i):
+    face = np.asarray(faces_test[index],dtype=np.uint8)
+    cv2.imwrite(path_save_faces,face)
+    nface = cv2.imread(path_save_faces)
+    
+    img_to_identify = face_recognition.face_encodings(nface)[0]
+    result = face_recognition.compare_faces([base_img_encoding], img_to_identify)
+    
+    if result[0]:
+        cv2.imshow("detected",nface)
+    else:
+        cv2.imshow("not detected",nface)
+    
+    os.remove(path_save_faces)
 
 cv2.waitKey(0)
-result = face_recognition.compare_faces([base_img_encoding], test_img_encoding)
-# cv2.imshow('test_img', test_img)
-
-print("Result: ", result)
 
 # # while True:
 #     _,img = captured.read()
@@ -64,4 +66,4 @@ print("Result: ", result)
 #         break
 
 captured.release()
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
