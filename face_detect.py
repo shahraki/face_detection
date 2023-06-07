14163369c3f77bf52ded2ccd2f3d412e87d1144b
Shahram Shahraki <shahram.shahraki@gmail.com>
2023-06-07 11:44:26 +0330

    make it like standard things

--- face_detect.py
+++ face_detect.py
@@ -2,6 +2,8 @@ import cv2
 import os
 import face_recognition
 import numpy as np
+import sys
+import argparse
 
 def idnetify_faces_image(face_number,test_faces,img_detected_base,show_detected):
     results=[]
@@ -83,25 +85,47 @@ def detect_face(base_img):
     
     return [i,faces_img]
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-p','--person_image',dest='person_image', type=str, help='Provide an image to find it in any other images or movies.')
+parser.add_argument('-t','--test_image',dest='test_image', type=str, help='Provide an image to find the person images in it.')
+parser.add_argument('-m','--test_movie',dest='test_movie', type=str, help='Provide an movie to find the person images in it.')
+args = parser.parse_args()
+
 cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
 haar_model = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
 face_cascade = cv2.CascadeClassifier(haar_model)
 path_save_faces = 'face.jpg'
 
-# To capture from camera
-# captured = cv2.VideoCapture(0)
+if not args.person_image:
+    print("--person_image is a mandatory argument. you may type -h for more help.")
+    sys.exit(0)
+else:
+    # base_img = cv2.imread("D:\\work\\github\\face-detect\\ID-Card 1.jpg")
+    base_img = cv2.imread(args.person_image.strip())
+    if base_img.any():
+        cv2.imshow("To find",base_img)
+        _,img_detected_base = detect_face(base_img)
+    else:
+        print("The main argument is empty.")
+        sys.exit(0)
 
-# To capture from file
-captured = cv2.VideoCapture("D:\\work\\github\\face-detect\\VID_20150718_134620.mp4")
+if args.test_image:
+    # test_img = cv2.imread("D:\\work\\github\\face-detect\\IMG_20150925_153809.jpg")
+    test_img = cv2.imread(args.test_image.strip())
+    i,imgs_detected_test = detect_face(test_img)
+    idnetify_faces_image(i, imgs_detected_test, img_detected_base,True)
 
-base_img = cv2.imread("D:\\work\\github\\face-detect\\ID-Card 1.jpg")
-test_img = cv2.imread("D:\\work\\github\\face-detect\\IMG_20150925_153809.jpg")
+if args.test_movie:
+    if args.test_movie == "camera":
+        # To capture from camera
+        captured = cv2.VideoCapture(0)
+    else:
+        # To capture from file
+        # captured = cv2.VideoCapture("D:\\work\\github\\face-detect\\VID_20150718_134620.mp4")
+        captured = cv2.VideoCapture(args.test_movie.strip())
+    
+    idnetify_faces_movie(captured,base_img)
+    captured.release()   
 
-_,img_detected_base = detect_face(base_img)
-i,imgs_detected_test = detect_face(test_img)
 
-idnetify_faces_image(i, imgs_detected_test, img_detected_base,True)    
-idnetify_faces_movie(captured,base_img)    
-
-captured.release()
 cv2.destroyAllWindows()
